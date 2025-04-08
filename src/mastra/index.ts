27a7f2de717739d080b3fd7d7aae8095724ee7f7
Omkar Bansod <omkarbansod221@gmail.com>
2025-04-09 00:21:25 +0530

    Modified CORS origin to use FRONTEND_URL environment variable

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:5173'], 
+  origin: process.env.FRONTEND_URL || ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://localhost:5173'], 
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
